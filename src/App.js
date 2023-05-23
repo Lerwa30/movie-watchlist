1772e463674a046f9296f36a25275a40acd63eb1
Leroy <leroypawlick@gmail.com>
2023-05-22 19:00:42 -0700

    set up state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [watchList, setWatchList] = useState([]);
+  const [page, setPage] = useState(1);
+
   return (
     <div className="App">
       <Header />
